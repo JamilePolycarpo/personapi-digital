f8ae919d8e72c84315e92e196e50c88468079483
Jamile <j.polycarpo@gmail.com>
2021-10-28 17:08:58 -0300

    Início Relatórios

--- src/test/java/one/digitalinnovation/personapi/PersonapiApplicationTests.java
+++ src/test/java/one/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -2,7 +2,7 @@ package one.digitalinnovation.personapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//teste
 @SpringBootTest
 class PersonapiApplicationTests {
 
